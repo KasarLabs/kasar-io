a07cb36864b8e0944371086ebb9719719151888b
antiyro <antiyro@gmail.com>
2025-03-13 15:50:17 +0000

    cleaned for build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"], weight: ["600"] });
 
